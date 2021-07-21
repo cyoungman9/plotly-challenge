7f0cc695555c580973377d86e73d68cc82953232
charles youngman <charlesyoungman@charless-MacBook-Air.local>
2021-07-21 19:18:51 -0400

    adding new files

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -6,6 +6,8 @@ function dropdown() {
       menu
         .append("option").text(sample).property("value", sample);
     });
+    buildtable(sampledata[0]),
+    buildcharts(sampledata[0])
   });
 }
 dropdown()
@@ -18,12 +20,12 @@ function buildtable(sample) {
     var table = d3.select("#sample-metadata");
     table.html("");
     console.log(filtereddata)
-  
-  Object.entries(filtereddata).forEach(function ([key, value]) {
+
+    Object.entries(filtereddata).forEach(function ([key, value]) {
       console.log(key, value);
       var row = table.append("tr");
-      row.append("td").html('${ key }`)
-      row.append('td').html('${ value }`);
+      row.append("td").html(key)
+      row.append('td').html(value)
     });
   });
 }
@@ -37,7 +39,45 @@ function buildcharts(sample) {
     var yvalue = filtereddata.sample_values
     var zvalue = filtereddata.otu_labels
 
-function optionChanged(newid){
+    var trace1 = {
+      y: xvalue.slice(0, 10).map(x => `OTU ${x}`).reverse(),
+      x: yvalue.slice(0, 10).reverse(),
+      text: zvalue.slice(0, 10).reverse(),
+      type: "bar", orientation: "h"
+    };
+
+    var data = [trace1];
+
+    var layout = {
+      title: "Top 10 Bacteria Cultures Found",
+    };
+
+    Plotly.newPlot("bar", data, layout);
+
+  // Bubble Chart
+
+  var trace2 = {
+    x: xvalue,
+    y: yvalue,
+    text: zvalue,
+    mode: "markers", 
+    marker: {
+      size: yvalue,color:xvalue
+    }
+  };
+
+  var data2 = [trace2];
+
+  var layout2 = {
+    title: "Bacteria Cultures Per Sample",
+    xaxis: { title: "OTU ID"}
+  };
+
+  Plotly.newPlot("bubble", data2, layout2);
+})
+};
+
+function optionChanged(newid) {
   buildtable(newid)
   buildcharts(newid)
-}
+};
